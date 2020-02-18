5576c6965ddf908a79feb050bffece3c22a30a42
David Roberts <droberts@livewatch.com>
2020-02-17 19:07:43 -0600

    validate only alphas rather than no spaces

--- app/models/word.rb
+++ app/models/word.rb
@@ -1,3 +1,12 @@
 class Word < ApplicationRecord
   validates :text, length: { minimum: 4 }
+  validate :only_alpha
+
+  private
+
+  def only_alpha
+    return unless text =~ /[^a-z]/
+    message = "text may only contain lowercase alpha characters"
+    errors.add(:text, message)
+  end
 end
--- spec/models/word_spec.rb
+++ spec/models/word_spec.rb
@@ -2,16 +2,31 @@ require "rails_helper"
 
 RSpec.describe Word, type: :model do
   describe "validations" do
-    describe "length" do
+    describe "text length" do
       context "less than four characters" do
         it "is invalid" do
-          expect(described_class.new(text: "hi")).to be_invalid
+          expect(build(:word, text: "hi")).to be_invalid
         end
       end
 
       context "four or more characters" do
         it "is valid" do
-          expect(described_class.new(text: "flop")).to be_valid
+          expect(build(:word, text: "flop")).to be_valid
+        end
+      end
+    end
+
+    describe "text content" do
+      context "with prohibited characters" do
+        [
+          "no pe",
+          "n0pe",
+          "NOPE",
+          "nope!",
+        ].each do |text|
+          it "is invalid" do
+            expect(build(:word, text: text)).to be_invalid
+          end
         end
       end
     end
