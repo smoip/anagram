6c6398a4df05daf55359355304694c96aaeb55e1
David Roberts <droberts@livewatch.com>
2020-02-17 18:40:25 -0600

    seed values

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+seed_text = File.read("#{Rails.root}/db/seed_text.txt")
+seed_text
+  .gsub("\n", "")
+  .downcase
+  .gsub(/[^a-z]/, " ")
+  .squish
+  .split(" ")
+  .each do |text|
+    Word.create(text: text)
+  end
+
