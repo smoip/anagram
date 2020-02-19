fb23a7bf3f2e1338f6fb626fed7ba60ea2dc47f2
David Roberts <droberts@livewatch.com>
2020-02-19 17:02:32 -0600

    add length to factory

--- spec/factories/word_factory.rb
+++ spec/factories/word_factory.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :word do
     text { "dessert" }
+    after(:build) do |word|
+      word.text_length = word.text.length if word.text_length.nil?
+    end
   end
 end
